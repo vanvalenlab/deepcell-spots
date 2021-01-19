e56ea63cd10bef20b8359425bb383f37ba00108c
Emily Laubscher <43769564+elaubsch@users.noreply.github.com>
2021-01-19 15:40:21 -0800

    Delete docker_start.sh

--- docker_start.sh
+++
@@ -1,6 +0,0 @@
-docker run --gpus '"device=0"'  -it \
-    -p 92:8888 \
-    -v /home/mly/deepcell-spots-new/deepcell_spots:/usr/local/lib/python3.6/dist-packages/deepcell_spots-new \
-    -v /home/mly/deepcell-spots-new/notebooks:/notebooks \
-    -v /home/mly/spot_training_data:/data \
-    mly/deepcell-spots-new:latest
