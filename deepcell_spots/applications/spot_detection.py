9d1c52cfd471eed952f5d0d182b8d36ccdbec733
Emily Laubscher <elaubsch@caltech.edu>
2021-07-19 16:24:37 -0700

    Fix import statement

--- deepcell_spots/applications/spot_detection.py
+++ deepcell_spots/applications/spot_detection.py
@@ -38,7 +38,7 @@ from deepcell_spots.preprocessing_utils import min_max_normalize
 from deepcell_spots.postprocessing_utils import y_annotations_to_point_list_max
 
 from deepcell_spots.applications.spots_application import Application
-from deepcell_spots.losses import DotNetLosses
+from deepcell_spots.dotnet_losses import DotNetLosses
 from deepcell_spots.dotnet import *
 
 MODEL_PATH = ('https://deepcell-data.s3-us-west-1.amazonaws.com/'
