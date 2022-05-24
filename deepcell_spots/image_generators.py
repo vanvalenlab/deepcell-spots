f2d70bb0f9ea2e52db2d2dfcd7e3e797ccc4df21
elaubsch <elaubsch@caltech.edu>
2022-05-24 13:21:10 -0700

    PEP8

--- deepcell_spots/image_generators.py
+++ deepcell_spots/image_generators.py
@@ -34,8 +34,8 @@ import numpy as np
 from tensorflow.keras.utils import to_categorical
 from tensorflow.python.keras import backend as K
 from tensorflow.keras.preprocessing.image import (ImageDataGenerator,
-                                                         Iterator,
-                                                         array_to_img)
+                                                  Iterator,
+                                                  array_to_img)
 
 from deepcell_spots.utils import (affine_transform_points,
                                   subpixel_distance_transform)
