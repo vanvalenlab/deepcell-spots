009fb47990aa66282b224ca959cb853727455d8a
elaubsch <elaubsch@caltech.edu>
2022-05-25 14:06:00 -0700

    Remove old tests

--- deepcell_spots/spot_em_test.py
+++ deepcell_spots/spot_em_test.py
@@ -30,10 +30,9 @@ import numpy as np
 import pandas as pd
 from tensorflow.python.platform import test
 
-from deepcell_spots.spot_em import (calc_tpr_fpr, cluster_coords,
-                                    consensus_coords, define_edges,
+from deepcell_spots.spot_em import (calc_tpr_fpr, define_edges,
                                     det_likelihood, em_spot,
-                                    norm_marg_likelihood, running_total_spots)
+                                    norm_marg_likelihood)
 
 
 class TestSpotEM(test.TestCase):
@@ -86,53 +85,22 @@ class TestSpotEM(test.TestCase):
         self.assertEqual(tp_likelihood, 1)
         self.assertEqual(fp_likelihood, 0)
 
-    # def test_em_spot(self):
-    #     num_clusters = 10
-    #     num_annotators = 3
-    #     cluster_matrix = np.ones((num_clusters, num_annotators))
-    #     tpr_list = np.ones(num_annotators)
-    #     fpr_list = np.zeros(num_annotators)
-    #     tp_list_new, fp_list_new, likelihood_matrix = em_spot(
-    #         cluster_matrix, tpr_list, fpr_list)
+    def test_em_spot(self):
+        num_clusters = 10
+        num_annotators = 3
+        cluster_matrix = np.ones((num_clusters, num_annotators))
+        tpr_list = np.ones(num_annotators)
+        fpr_list = np.zeros(num_annotators)
+        tp_list_new, fp_list_new, likelihood_matrix = em_spot(
+            cluster_matrix, tpr_list, fpr_list)
 
-    #     self.assertEqual(len(tpr_list), len(tp_list_new))
-    #     self.assertEqual(tpr_list.all(), 1)
-    #     self.assertEqual(len(fpr_list), len(fp_list_new))
-    #     self.assertEqual(fpr_list.all(), 0)
-    #     self.assertEqual(np.shape(cluster_matrix)[
-    #                      0], np.shape(likelihood_matrix)[0])
-    #     self.assertEqual(likelihood_matrix.all(), 1)
-
-    # def test_cluster_coords(self):
-    #     num_detections = 10
-    #     num_annotators = 3
-    #     num_images = 10
-    #     image_dim = 128
-    #     coords = np.random.random_sample(
-    #         (num_annotators, num_images, num_detections, 2))
-    #     image_stack = np.random.random_sample(
-    #         (num_images, image_dim, image_dim))
-    #     threshold = 1
-    #     cluster_matrix, centroid_list, coords_updated, image_stack_updated = cluster_coords(
-    #         coords, image_stack, threshold)
-
-    #     self.assertEqual(len(cluster_matrix), len(np.vstack(centroid_list)))
-    #     self.assertEqual(np.shape(cluster_matrix)[1], np.shape(coords)[0])
-    #     self.assertLessEqual(np.shape(coords_updated)[1], num_images)
-    #     self.assertLessEqual(np.shape(image_stack_updated)[0], num_images)
-    #     self.assertEqual(np.shape(coords_updated)[0], num_annotators)
-    #     self.assertEqual(np.shape(coords_updated)[2], num_detections)
-    #     self.assertEqual(np.shape(image_stack_updated)[1], image_dim)
-    #     self.assertEqual(np.shape(image_stack_updated)[2], image_dim)
-
-    # def test_running_total_spots(self):
-    #     num_images = 10
-    #     num_detections = 10
-    #     centroid_list = np.random.random_sample(
-    #         (num_images, num_detections, 2))
-    #     running_total = running_total_spots(centroid_list)
-
-    #     self.assertEqual(len(running_total), num_images + 1)
+        self.assertEqual(len(tpr_list), len(tp_list_new))
+        self.assertEqual(tpr_list.all(), 1)
+        self.assertEqual(len(fpr_list), len(fp_list_new))
+        self.assertEqual(fpr_list.all(), 0)
+        self.assertEqual(np.shape(cluster_matrix)[
+                         0], np.shape(likelihood_matrix)[0])
+        self.assertEqual(likelihood_matrix.all(), 1)
 
     def test_define_edges(self):
         # check shape of output
@@ -173,17 +141,6 @@ class TestSpotEM(test.TestCase):
         threshold = 0
         A = define_edges(coords_df, threshold)
         self.assertEqual(A.all(), np.zeros((2, 2)).all())
-
-    # def test_consensus_coords(self):
-    #     num_clusters = 10  # per image
-    #     num_images = 10
-    #     p_matrix = np.random.random_sample((num_clusters * num_images, 2))
-    #     centroid_list = np.random.random_sample((num_images, num_clusters, 2))
-    #     running_total = np.arange(0, 110, 10)
-    #     y = consensus_coords(p_matrix, centroid_list, running_total)
-
-    #     self.assertLessEqual(len(y), len(centroid_list))
-    #     self.assertLessEqual(len(y), len(running_total) - 1)
 
 
 if __name__ == '__main__':
