beb8b713285b087d331990f6444486b0962e9ac2
elaubsch <elaubsch@caltech.edu>
2022-06-09 12:24:00 -0700

    Update settings for RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,31 +10,76 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+from datetime import datetime
+import mock
+from sphinx.builders.html import StandaloneHTMLBuilder
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'Deepcell Spots'
-copyright = '2022, Van Valen Lab'
-author = 'Van Valen Lab'
+copyright = ('2016-{currentyear}, Van Valen Lab at the '
+             'California Institute of Technology (Caltech)').format(
+                 currentyear=datetime.now().year)
+author = 'Van Valen Lab at Caltech'
 
 # The full version, including alpha/beta/rc tags
 release = '0.3.1'
 
+# -- RTD configuration ------------------------------------------------
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+
+# This is used for linking and such so we link to the thing we're building
+rtd_version = os.environ.get("READTHEDOCS_VERSION", "master")
+if rtd_version not in ["stable", "latest", "master"]:
+    rtd_version = "stable"
+
 
 # -- General configuration ---------------------------------------------------
+
+# If your documentation needs a minimal Sphinx version, state it here.
+needs_sphinx = '2.3.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'm2r',
+    'IPython.sphinxext.ipython_console_highlighting',
+    'nbsphinx',
+    'nbsphinx_link',
+    'sphinx.ext.todo',
+    'sphinx.ext.autosectionlabel'
 ]
+
+napoleon_google_docstring = True
+
+default_role = 'py:obj'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = ['.rst', '.md']
+
+# Ignore warning:
+suppress_warnings = ['autosectionlabel.*']
+
+# The master toctree document.
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
