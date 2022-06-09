93cf0eb3c7ce56bca72e54793d40cc0353db3b3d
elaubsch <elaubsch@caltech.edu>
2022-06-09 13:49:09 -0700

    PEP8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,7 +86,6 @@ master_doc = 'index'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
