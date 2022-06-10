97a198048f7a85a0eed9acc92c296e5cd0c02b3a
elaubsch <elaubsch@caltech.edu>
2022-06-10 13:29:55 -0700

    Update docstrings

--- deepcell_spots/image_generators.py
+++ deepcell_spots/image_generators.py
@@ -42,27 +42,30 @@ from deepcell_spots.utils import (affine_transform_points,
 
 
 class ImageFullyConvDotIterator(Iterator):
-    """Iterator yielding data from Numpy arrays (`X and `y`).
+    """Iterator yielding data from Numpy arrays (`X` and `y`).
 
     Args:
-        train_dict: dictionary consisting of numpy arrays for `X` and `y`.
-            X - (batch, Ly, Lx, channel)
-            y - np.array of length batch containing np.arrays of shape (N, 2)
-            N = # of points in the image
-        image_data_generator: Instance of `ImageDataGenerator`
-            to use for random transformations and normalization.
-        batch_size: Integer, size of a batch.
-        shuffle: Boolean, whether to shuffle the data between epochs.
-        seed: Random seed for data shuffling.
-        data_format: String, one of `channels_first`, `channels_last`.
-        save_to_dir: Optional directory where to save the pictures
-            being yielded, in a viewable format. This is useful
-            for visualizing the random transformations being
-            applied, for debugging purposes.
-        save_prefix: String prefix to use for saving sample
-            images (if `save_to_dir` is set).
-        save_format: Format to use for saving sample images
-            (if `save_to_dir` is set).
+        train_dict (dict): dictionary consisting of numpy arrays for `X` and `y`.
+            X has dimensions (batch, Ly, Lx, channel) and y has length batch
+            containing np.arrays of shape (N, 2) where N is the number of
+            points in the image.
+        image_data_generator: Instance of `ImageDataGenerator` to use for random
+            transformations and normalization.
+        batch_size (int): Size of a batch.
+        shuffle (bool): Whether to shuffle the data between epochs.
+        seed (int): Random seed for data shuffling.
+        data_format (str): One of (`channels_first`, `channels_last`).
+        save_to_dir (str): Optional directory where to save the pictures
+            being yielded, in a viewable format. This is useful for visualizing
+            the random transformations being applied, for debugging purposes.
+        save_prefix (str): Prefix to use for saving sample images (if `save_to_dir`
+            is set).
+        save_format (str): Format to use for saving sample images (if `save_to_dir`
+            is set).
+
+    Raises:
+        ValueError: Training batches and labels should have the same length.
+        ValueError: Input data in `ImageFullyConvIterator` should have rank 4.
     """
 
     def __init__(self,
@@ -102,33 +105,20 @@ class ImageFullyConvDotIterator(Iterator):
             self.x.shape[0], batch_size, shuffle, seed)
 
     def point_list_to_annotations(self, points, image_shape, dy=1, dx=1):
-        """ Generate label images used in loss calculation from point labels
+        """ Generate label images used in loss calculation from point labels.
 
         Args:
-            points:
-            (N, 2) numpy array which contains points in the format [y, x]
-
-            image_shape:
-                tuple of length 2 of the image shape
-
-            dy:
-                pixel y width
-
-            dx:
-                pixel x width
+            points (np.array): array of size (N, 2) which contains points in the format [y, x].
+            image_shape (tuple): shape of 2-dimensional image.
+            dy: pixel y width.
+            dx: pixel x width.
 
         Returns:
-            annotations dictionary:
-            detections:
-            numpy array of shape (image_shape,2), with (i,j,:) being a one-hot encoding of the
-            classification of each pixel as containing a point in points, or not containing one.
-            offset: two stacked images with the shape of the input image
-            delta_x:
-                numpy array of shape image_shape, each pixel is = the signed x distance between
-                a point from points that is near pixel [i,j] and the center of the pixel
-            delta_y:
-                numpy array of shape image_shape, each pixel is = the signed y distance between
-                a point from points that is near pixel [i,j] and the center of the pixel
+            annotations (dict): Dictionary with two keys, `detections` and `offset`.
+                - `detections` is array of shape (image_shape,2) with pixels one hot encoding
+                  spot locations.
+                - `offset` is array of shape (image_shape,2) with pixel values equal to
+                  signed distance to nearest spot in x- and y-directions.
         """
 
         contains_point = np.zeros(image_shape)
@@ -143,14 +133,11 @@ class ImageFullyConvDotIterator(Iterator):
 
         one_hot_encoded_cp = to_categorical(contains_point)
 
-        annotations = {'detections': one_hot_encoded_cp, 'offset': offset}
+        annotations = {'detections': one_hot_encoded_cp, 'offset': offset}  # TODO: refactor detections to classification
         return annotations
 
     def _get_batches_of_transformed_samples(self, index_array):
         batch_x = np.zeros(tuple([len(index_array)] + list(self.x.shape)[1:]))
-        # batch_y = np.zeros(tuple([len(index_array)] + list(self.y.shape)[1:]))
-        # DELETE batch_y? its size above is not true - y is a point list but batch output will be
-        # image*3 size
 
         batch_detections = []  # eventual shape: (batch, Lx, Ly, 2)
         batch_offset = []  # eventual shape: (batch, Lx, Ly, 2)
@@ -248,60 +235,61 @@ class ImageFullyConvDotIterator(Iterator):
 
 class ImageFullyConvDotDataGenerator(ImageDataGenerator):
     """Generates batches of tensor image data with real-time data augmentation.
-    The data will be looped over (in batches).
+    
+    The data will be looped over in batches.
 
     Args:
-        featurewise_center: boolean, set input mean to 0 over the dataset,
-            feature-wise.
-        samplewise_center: boolean, set each sample mean to 0.
-        featurewise_std_normalization: boolean, divide inputs by std
+        featurewise_center (bool): Whether to set input mean to 0 over the
+            dataset, feature-wise.
+        samplewise_center (bool): Whether to set each sample mean to 0.
+        featurewise_std_normalization (bool): Whether to divide inputs by std
             of the dataset, feature-wise.
-        samplewise_std_normalization: boolean, divide each input by its std.
-        zca_epsilon: epsilon for ZCA whitening. Default is 1e-6.
-        zca_whitening: boolean, apply ZCA whitening.
-        rotation_range: int, degree range for random rotations.
+        samplewise_std_normalization (bool): Whether to divide each input by
+            its std.
+        zca_epsilon (float): Epsilon for ZCA whitening. Default is 1e-6.
+        zca_whitening (bool): Whether to apply ZCA whitening.
+        rotation_range (int): Degree range for random rotations.
         width_shift_range: float, 1-D array-like or int
-            float: fraction of total width, if < 1, or pixels if >= 1.
-            1-D array-like: random elements from the array.
-            int: integer number of pixels from interval
-                `(-width_shift_range, +width_shift_range)`
-            With `width_shift_range=2` possible values are ints [-1, 0, +1],
-            same as with `width_shift_range=[-1, 0, +1]`,
-            while with `width_shift_range=1.0` possible values are floats in
-            the interval [-1.0, +1.0).
-        shear_range: float, shear Intensity
-            (Shear angle in counter-clockwise direction in degrees)
+            - float: fraction of total width, if < 1, or pixels if >= 1.
+            - 1-D array-like: random elements from the array.
+            - int: integer number of pixels from interval
+                `(-width_shift_range, +width_shift_range)` with
+                `width_shift_range=2` possible values are ints [-1, 0, +1],
+                same as with `width_shift_range=[-1, 0, +1]`, while with
+                `width_shift_range=1.0` possible values are floats in
+                the interval [-1.0, +1.0).
+        shear_range (float): Shear angle in counter-clockwise direction in
+            degrees
         zoom_range: float or [lower, upper], Range for random zoom.
             If a float, `[lower, upper] = [1-zoom_range, 1+zoom_range]`.
-        channel_shift_range: float, range for random channel shifts.
-        fill_mode: One of {"constant", "nearest", "reflect" or "wrap"}.
-            Default is 'nearest'. Points outside the boundaries of the input
+        channel_shift_range (float): Range for random channel shifts.
+        fill_mode (str): One of `("constant", "nearest", "reflect" or "wrap")`.
+            Default is `"nearest"`. Points outside the boundaries of the input
             are filled according to the given mode:
-                'constant': kkkkkkkk|abcd|kkkkkkkk (cval=k)
-                'nearest':  aaaaaaaa|abcd|dddddddd
-                'reflect':  abcddcba|abcd|dcbaabcd
-                'wrap':  abcdabcd|abcd|abcdabcd
+                - 'constant': kkkkkkkk|abcd|kkkkkkkk (cval=k)
+                - 'nearest':  aaaaaaaa|abcd|dddddddd
+                - 'reflect':  abcddcba|abcd|dcbaabcd
+                - 'wrap':  abcdabcd|abcd|abcdabcd
         cval: float or int, value used for points outside the boundaries
             when `fill_mode = "constant"`.
-        horizontal_flip: boolean, randomly flip inputs horizontally.
-        vertical_flip: boolean, randomly flip inputs vertically.
+        horizontal_flip (bool): Whether to randomly flip inputs horizontally.
+        vertical_flip (bool): Whether to randomly flip inputs vertically.
         rescale: rescaling factor. Defaults to None. If None or 0, no rescaling
             is applied, otherwise we multiply the data by the value provided
             (before applying any other transformation).
         preprocessing_function: function that will be implied on each input.
             The function will run after the image is resized and augmented.
-            The function should take one argument:
-            one image (Numpy tensor with rank 3),
-            and should output a Numpy tensor with the same shape.
-        data_format: One of {"channels_first", "channels_last"}.
-            "channels_last" mode means that the images should have shape
-                `(samples, height, width, channels)`,
-            "channels_first" mode means that the images should have shape
-                `(samples, channels, height, width)`.
-            It defaults to the `image_data_format` value found in your
-                Keras config file at `~/.keras/keras.json`.
-            If you never set it, then it will be "channels_last".
-        validation_split: float, fraction of images reserved for validation
+            The function should take one image (Numpy tensor with rank 3) as
+            argument and should output a Numpy tensor with the same shape.
+        data_format: One of `("channels_first", "channels_last")`.
+            `"channels_last"` means that the images should have shape
+            `(samples, height, width, channels)`, `"channels_first"`
+            means that the images should have shape
+            `(samples, channels, height, width)`. It defaults to the
+            `image_data_format` value found in your Keras config file at
+            `~/.keras/keras.json`. If you never set it, then it will be
+            `"channels_last"`.
+        validation_split (float): Fraction of images reserved for validation
             (strictly between 0 and 1).
     """
 
@@ -319,18 +307,17 @@ class ImageFullyConvDotDataGenerator(ImageDataGenerator):
         """Generates batches of augmented/normalized data with given arrays.
 
         Args:
-            train_dict: dictionary of X and y tensors. Both should be rank 4.
-            batch_size: int (default: 1).
-            shuffle: boolean (default: True).
-            seed: int (default: None).
-            save_to_dir: None or str (default: None).
-                This allows you to optionally specify a directory
-                to which to save the augmented pictures being generated
-                (useful for visualizing what you are doing).
-            save_prefix: str (default: `''`). Prefix to use for filenames of
-                saved pictures (only relevant if `save_to_dir` is set).
-            save_format: one of "png", "jpeg". Default: "png".
-                (only relevant if `save_to_dir` is set)
+            train_dict (dict): dictionary of X and y tensors. Both should be rank 4.
+            batch_size (int): Size of a batch.
+            shuffle (bool): Whether to shuffle the data between epochs.
+            seed (int): Random seed for data shuffling.
+            save_to_dir (str): Optional directory where to save the pictures
+                being yielded, in a viewable format. This is useful for visualizing
+                the random transformations being applied, for debugging purposes.
+            save_prefix (str): Prefix to use for saving sample images (if `save_to_dir`
+                is set).
+            save_format (str): Format to use for saving sample images (if `save_to_dir`
+                is set).
 
         Returns:
             An Iterator yielding tuples of `(x, y)` where `x` is a numpy array
@@ -355,27 +342,24 @@ class ImageFullyConvDotDataGenerator(ImageDataGenerator):
         given parameters.
 
         Args:
-            y: (N, 2) numpy array which contains points in the format [y, x] or list of such arrays
-            (y Cartesian coordinate before the x, as in matrix/image indexing convention.
-            Not to confuse with the variables X,y - data and labels)
-
-            transform_parameters: Dictionary with string - parameter pairs
+            y (np.array): Array of shape (N, 2) which contains points in the format [y, x]
+                or list of such arrays. (y Cartesian coordinate before the x, as in
+                matrix/image indexing convention. Not to be confused with the variables X,y
+                as in data and labels)
+            transform_parameters (dict): Dictionary with string - parameter pairs
                 describing the transformation.
-                Currently, the following parameters
-                from the dictionary are used:
-                - `'theta'`: Float. Rotation angle in degrees.
-                - `'tx'`: Float. Shift in the x direction.
-                - `'ty'`: Float. Shift in the y direction.
-                - `'shear'`: Float. Shear angle in degrees.
-                - `'zx'`: Float. Zoom in the x direction.
-                - `'zy'`: Float. Zoom in the y direction.
-                - `'flip_horizontal'`: Boolean. Horizontal flip.
-                - `'flip_vertical'`: Boolean. Vertical flip.
-                - `'channel_shift_intensity'`: Float. Channel shift intensity.
-                - `'brightness'`: Float. Brightness shift intensity.
-                (taken from: keras ImageDataGenerator documentation)
-
-            image_shape: tuple of length 2 of the image shape
+                    - `'theta'`: Float. Rotation angle in degrees.
+                    - `'tx'`: Float. Shift in the x direction.
+                    - `'ty'`: Float. Shift in the y direction.
+                    - `'shear'`: Float. Shear angle in degrees.
+                    - `'zx'`: Float. Zoom in the x direction.
+                    - `'zy'`: Float. Zoom in the y direction.
+                    - `'flip_horizontal'`: Boolean. Horizontal flip.
+                    - `'flip_vertical'`: Boolean. Vertical flip.
+                    - `'channel_shift_intensity'`: Float. Channel shift intensity.
+                    - `'brightness'`: Float. Brightness shift intensity.
+                      (taken from: keras ImageDataGenerator documentation)
+            image_shape (tuple): shape of 2-dimensional image.
         """
 
         # x is a single image, so it doesn't have image number at index 0
@@ -392,33 +376,32 @@ class ImageFullyConvDotDataGenerator(ImageDataGenerator):
         return y
 
     def random_transform(self, x, y=None, seed=None):
-        """Applies a random transformation to an image,
-        and (optional) to point labels referring to coordinates in the image - with -0.5 <= x <= Lx,
-        -0.5 <= y <= Ly, for Ly, Lx = x.shape[0,1]
+        """Applies a random transformation to an image
 
         Args:
-            x: 3D tensor or list of 3D tensors,
-                single image.
-            y: (N, 2) numpy array which contains points in the format [y, x] or list of such arrays
-                referring to coordinates in the image `x`, optional.
+            x: 3D tensor or list of 3D tensors.
+            y (np.array): Array of shape (N, 2) which contains points in the format [y, x]
+                or list of such arrays referring to coordinates in the image `x`, optional.
             seed: Random seed.
-            fill_mode: type of padding used for points outside of the input image which correspond
-            to points inside the output image. One of {"constant", "nearest", "reflect" or "wrap"}.
+            fill_mode (str): One of `("constant", "nearest", "reflect" or "wrap")` for type
+                of padding used for points outside of the input image which correspond to
+                points inside the output image. Points outside the boundaries of the input
+                are filled according to the given mode:
+                    - 'constant': kkkkkkkk|abcd|kkkkkkkk (cval=k)
+                    - 'nearest':  aaaaaaaa|abcd|dddddddd
+                    - 'reflect':  abcddcba|abcd|dcbaabcd
+                    - 'wrap':  abcdabcd|abcd|abcdabcd
 
         Returns:
-            A randomly transformed version of the input (same shape).
-            If `y` is passed, it is transformed if necessary and returned.
-            the transformed y contains input and padding (for fill_mode='reflect' or 'wrap') points
-            mapped to output image space, which are inside the output image (transformed points
-            mapped to outside of the output image boundaries are deleted)
-
-
-            NOTICE: NO CHANNEL SUPPORT HERE! CAN ADD LATER (SEE OLDER BUGGY FILE -
-            need to decide how to save y with channels, could be (batch, channel, N, 2) or switch
-            batch and channel.
+            A randomly transformed version of the input (same shape). If `y` is passed, it is
+            transformed if necessary and returned. The transformed y contains input and padding
+            (for fill_mode='reflect' or 'wrap') points mapped to output image space, which are
+            inside the output image (transformed points mapped to outside of the output image
+            boundaries are deleted)
         """
         params = self.get_random_transform(x.shape, seed)
 
+        # TODO
         # channel support code below (?) commented out for now
         # if isinstance(x, list):
         #    x = [self.apply_transform(x_i, params) for x_i in x]
