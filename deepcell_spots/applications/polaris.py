39351d59fca725ca37cba96b8401603e55b8f74a
xuefei-wang <wang.xuefei.wang@gmail.com>
2022-11-23 15:57:38 -0800

    PEP8 continued again

--- deepcell_spots/applications/polaris.py
+++ deepcell_spots/applications/polaris.py
@@ -265,7 +265,7 @@ class Polaris(object):
             output_image, spots_locations, extra_pixel_num=maxpool_extra_pixel_num)
         spots_intensities_vec = np.concatenate(spots_intensities)
         spots_locations_vec = np.concatenate([np.concatenate([item, [[idx_batch]] * len(item)],
-                                                             axis=1) for idx_batch, item in enumerate(spots_locations)])
+                                axis=1) for idx_batch, item in enumerate(spots_locations)])
 
         if segmentation_image is not None:
             if not self.segmentation_app:
