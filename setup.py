19a38b48749854ab14790e84251622327f53ef78
Ross Barnowski <rossbar@caltech.edu>
2024-01-26 11:43:10 -0800

    Rm distutils fallback.

--- setup.py
+++ setup.py
@@ -26,11 +26,7 @@
 import os
 
 from codecs import open
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 
 here = os.path.abspath(os.path.dirname(__file__))
